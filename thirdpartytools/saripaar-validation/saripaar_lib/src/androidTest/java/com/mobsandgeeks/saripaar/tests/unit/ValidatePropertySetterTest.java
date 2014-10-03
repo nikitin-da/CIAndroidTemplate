5280cbd4ffcf529e57e90a888de5d2455728de34
Dmitry Nikitin <Dmitry_Nikitin@epam.com>
2014-10-03 13:51:31 +0400

    [build][refactor][test] Prepare saripaar for tests
    Make canonical gradle form for saripaar library
    Add simple unit test

---
+++ thirdpartytools/saripaar-validation/saripaar_lib/src/androidTest/java/com/mobsandgeeks/saripaar/tests/unit/ValidatePropertySetterTest.java
@@ -0,0 +1,32 @@
+package com.mobsandgeeks.saripaar.tests.unit;
+
+import android.test.suitebuilder.annotation.SmallTest;
+
+import com.mobsandgeeks.saripaar.Validator;
+
+import junit.framework.TestCase;
+
+/**
+ * ApplicationSessionManager test.
+ */
+@SmallTest
+public class ValidatePropertySetterTest extends TestCase {
+
+    /**
+     *  Simple test.
+     *  Added, because CI report with Jococo failed,
+     *  when there is no tests in submodule.
+     */
+    public void testPropertySetter() {
+        Validator validator = new Validator(this);
+        String name = "property_name";
+        String firstValue = "first_value";
+        String secondValue = "second_value";
+
+        validator.setProperty(name, firstValue);
+        assertEquals(firstValue, validator.getProperty(name));
+
+        validator.setProperty(name, secondValue);
+        assertEquals(secondValue, validator.getProperty(name));
+    }
+}
